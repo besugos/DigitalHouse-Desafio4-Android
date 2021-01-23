c854dc53b4c42f029c09d42c4ed6b29fb8a13750
Wendel Bandeira <besugos@gmail.com>
2021-01-23 20:55:49 -0300

    Layout tela de detalhes

---
+++ app/src/main/java/com/besugos/desafio4dha/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.besugos.desafio4dha.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.besugos.desafio4dha.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
