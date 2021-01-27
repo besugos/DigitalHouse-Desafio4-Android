880d177c20d65c2a1a4215f2445135388a3a54e3
Wendel Bandeira <besugos@gmail.com>
2021-01-26 23:27:18 -0300

    Separa a gravacao e leitura de dados por usuario

--- app/src/main/java/com/besugos/desafio4dha/detail/DetailActivity.kt
+++ app/src/main/java/com/besugos/desafio4dha/detail/DetailActivity.kt
@@ -3,14 +3,9 @@ package com.besugos.desafio4dha.detail
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.besugos.desafio4dha.R
-import com.besugos.desafio4dha.edit.view.EditActivity
-import com.besugos.desafio4dha.home.view.HomeActivity
+import com.besugos.desafio4dha.edit.view.AddActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
-import com.google.firebase.auth.FirebaseAuth
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,7 +14,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         findViewById<FloatingActionButton>(R.id.fabEdit).setOnClickListener {
-            val intent = Intent(this, EditActivity::class.java)
+            val intent = Intent(this, AddActivity::class.java)
             startActivity(intent)
 
 
